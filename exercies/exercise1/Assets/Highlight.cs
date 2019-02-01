facd57fade3fae620ca9f3e6742e2b2039913ee7
SAligames <sa3462a@student.american.edu>
2019-01-31 21:15:36 -0500

    Unity Projects
    I'm still learning Github

---
+++ exercies/exercise1/Assets/Highlight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlight : MonoBehaviour
+{
+
+    public Material highlightMaterial;
+    Material original;
+    public float highlightTime = 5;
+    float highlightExpireTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        original = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > highlightExpireTime)
+        {
+            // Reset the original material.
+            GetComponent<Renderer>().material = original;
+        }
+    }
+    public void Highlighter()
+    {
+        GetComponent<Renderer>().material = highlightMaterial;
+        highlightExpireTime = Time.time + highlightTime;
+    }
+}
