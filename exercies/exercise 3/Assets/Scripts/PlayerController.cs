0dd639e5329c70f4064dc3b445ac3077aee3bb72
SAligames <sa3462a@student.american.edu>
2019-02-05 23:09:18 -0500

    All done

--- exercies/exercise 3/Assets/Scripts/PlayerController.cs
+++ exercies/exercise 3/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
     private Rigidbody rb;
+    public Text scoreText;
+    public Text winText;
+    private int score;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        score = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     private void FixedUpdate()
@@ -28,6 +35,17 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            score = score + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        scoreText.text = "Score:" + score.ToString();
+        if(score==10)
+        {
+            winText.text = "Strike!";
         }
     }
 }
