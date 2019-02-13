23d61b064b781a36cc455dd36de911905ad0cc3b
SAligames <sa3462a@student.american.edu>
2019-02-13 10:09:26 -0500

    Roll a ball to score by collecting

---
+++ exercises/exercise04/Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    public Text scoreText;
+     
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+        SetCountText();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement*speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            score = score + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        scoreText.text = "Score:" + score.ToString();
+    }
+  
+}
