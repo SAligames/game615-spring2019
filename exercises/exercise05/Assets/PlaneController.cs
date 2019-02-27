443461d967cf46bef186ee5a07361211740f3927
Sharmarke <sharmarkeali239@gmail.com>
2019-02-27 16:23:34 -0500

    Drone flying over terrain
    Drone can be controlled and collides when crashing into the terrain causing the drone is bounce off. However, I failed to earn a point when flying throught a ring.

--- exercises/exercise05/Assets/PlaneController.cs
+++ exercises/exercise05/Assets/PlaneController.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlaneController : MonoBehaviour
 {
+    float cameraFollowBehindAmount = 50;
+    float cameraFollowAboveAmount = 25;
+    int score;
+    public Text scoreText;
+    private Rigidbody rb;
+    public Collider box;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("plane pilot script added to:" + gameObject.name);
+        rb = GetComponent<Rigidbody>();
+        box = GetComponent<Collider>();
+        score = 0;
+        SetCountText();
     }
 
     // Update is called once per frame
@@ -16,5 +28,30 @@ public class PlaneController : MonoBehaviour
         transform.position += transform.forward * Time.deltaTime * 90.0f;
         transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
 
+        Camera.main.transform.position = transform.position + -transform.forward * cameraFollowBehindAmount + Vector3.up * cameraFollowAboveAmount;
+        Camera.main.transform.LookAt(transform);
+
+        float terrainHeightWhereWeAre = Terrain.activeTerrain.SampleHeight(transform.position);
+
+        if(terrainHeightWhereWeAre>transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, terrainHeightWhereWeAre, transform.position.z);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            Destroy(other.gameObject);
+            score = score + 1;
+            SetCountText();
+        }            
+        
+    }
+
+    void SetCountText()
+    {
+        scoreText.text = "Score:" + score.ToString();
     }
 }
