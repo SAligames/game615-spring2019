4a96bf032fd892b9dd3f1c9195f0c01d0fbfdbb2
SAligames <sa3462a@student.american.edu>
2019-02-27 10:15:01 -0500

    Imported a drone model

---
+++ exercises/exercise05/Assets/PlaneController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * 90.0f;
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+
+    }
+}
