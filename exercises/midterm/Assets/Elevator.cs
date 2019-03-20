3fb899083d8fa8c4e27d9f6e7ae03adf5622b739
Sharmarke <sharmarkeali239@gmail.com>
2019-03-20 17:09:02 -0400

    More and more changes

---
+++ exercises/midterm/Assets/Elevator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public float timePerCycle = 1;
+    public float magnitude = 10;
+
+    Vector3 initialPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the target with a smooth cosine wave
+        var y = magnitude * Mathf.Cos(Time.time / timePerCycle * 2 * Mathf.PI);
+        transform.position = initialPosition + Vector3.up * y;
+    }
+}
