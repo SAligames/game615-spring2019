6eb63538edba292c12330f71b5b49f2b5680f894
SAligames <sa3462a@student.american.edu>
2019-04-12 10:35:30 -0400

    Working on player movement

---
+++ exercises/final/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    float horizontalMove=0f;
+    public float runSpeed = 40f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    }
+
+}
