0ecf4277132e06b74f62a8af25ae54f5fe1c7ae9
SAligames <sa3462a@student.american.edu>
2019-04-16 22:38:23 -0400

    Camera follows player

--- exercises/final/Assets/Script/CameraFollow.cs
+++ exercises/final/Assets/Script/CameraFollow.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     private Transform playerTransform;
+    public float offset;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        Vector3 temp = transform.position; //Store current camera position in variable temp-temporary position
+        temp.x = playerTransform.position.x; //Set the camera's position x to be equal to the player's position x when moving
+        temp.y = playerTransform.position.y; //Set the camera's position y to be equal to the player's position y when jumping
+        //temp.x += offset; //Add the offset value to the temporary position
+        transform.position = temp; //Set back the camera's temp position to the position to the camera's current position
+
     }
 }
