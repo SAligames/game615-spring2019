fc4fa186716b32ef0e5cad7c5c2d4fc6a2d17fae
Sharmarke <sharmarkeali239@gmail.com>
2019-04-26 16:07:53 -0400

    Health UI

---
+++ exercises/final/Assets/Script/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Slider healthBar;
+    private float maxHealth = 100;
+    private float currentHealth;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar.value = maxHealth;
+        currentHealth = maxHealth;
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.gameObject.tag=="Damage")
+        {
+            healthBar.value -= 1f;
+            currentHealth = healthBar.value;
+            if(currentHealth==0)
+            {
+                SceneManager.LoadScene("LoseScreen");
+            }
+        }
+    }
+}
